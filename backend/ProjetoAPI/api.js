07084c92aa0886cf98e8be07d77cb816d27b6ac8
Bruno Santos <brunodsp.dev@gmail.com>
2025-10-23 22:38:13 -0300

    Atualizar P

--- backend/ProjetoAPI/api.js
+++ backend/ProjetoAPI/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cadastro-de-usuarios-4cpr.onrender.com", // ajuste a URL do seu backend
+  baseURL: "https://cadastro-de-usuarios-4cpr.onrender.com/usuarios", // ajuste a URL do seu backend
 });
 
 export default api;
