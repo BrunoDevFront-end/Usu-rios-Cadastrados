c99959f6f802af19108254f2d80917f045230ed9
Bruno Santos <brunodsp.dev@gmail.com>
2025-10-23 21:46:52 -0300

    Atualizando projeto

--- backend/ProjetoAPI/api.js
+++ backend/ProjetoAPI/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 // Cria instância do Axios apontando para o backend local
 const api = axios.create({
-  baseURL: "http://localhost:3000", // URL base da sua API
+  baseURL: "https://cadastro-de-usuarios-4cpr.onrender.com", // URL base da sua API
 });
 
 export default api;
